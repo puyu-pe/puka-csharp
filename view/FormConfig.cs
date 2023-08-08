311212b7be3a96c3b53838549c0b8cc3da129ae6
OsLan99 <161139@unamba.edu.pe>
2023-08-08 18:06:31 -0500

    BUGFIX solucion cierre inmediato
    La aplicación ya no se cierra de forma inmediata cuando hace click en
    aceptar luego de llenar sus datos, ahora solo se cierra si el usuario
    hace click explicitamente en el boton de cierre

--- view/FormConfig.cs
+++ view/FormConfig.cs
@@ -11,81 +11,82 @@ using System.Windows.Forms;
 
 namespace puka.view
 {
-    public partial class FormConfig : Form
+  public partial class FormConfig : Form
+  {
+    private string pathFileImage = "";
+
+    public FormConfig()
     {
-        private string pathFileImage = "";
-
-        public FormConfig()
-        {
-            InitializeComponent();
-        }
-
-        public string GetUrlBifrostServer()
-        {
-            if (!string.IsNullOrEmpty(pathFileImage))
-            {
-                string fileDestiny = Path.Combine(Directory.GetCurrentDirectory(), "img");
-
-                createFolderImage();
-
-                string nameFileDestiny = Path.Combine(fileDestiny, "logo" + Path.GetExtension(pathFileImage));
-
-                try
-                {
-                    if (File.Exists(nameFileDestiny))
-                    {
-                        File.Delete(nameFileDestiny);
-                    }
-
-                    File.Copy(pathFileImage, nameFileDestiny);
-                    MessageBox.Show("Imagen guardada correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error al guardar la imagen: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-
-            string ruc = textRUC.Text.Trim();
-            string suffixCompany = textSuffix.Text.Trim();
-            string domainBifrost = textDomain.Text.Trim();
-            string namespaceBifrost = textNamespace.Text.Trim();
-            return String.Format("{0}/{1}-{2}-{3}", domainBifrost, namespaceBifrost, ruc, suffixCompany);
-        }
-
-        private void OnCancel(object sender, EventArgs e)
-        {
-            Dispose();
-            Application.Exit();
-        }
-
-        private void OnClose(object sender, FormClosingEventArgs e)
-        {
-            //Dispose();
-            Application.Exit();
-        }
-
-        private void btnPathImage_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Archivo de imagen (*.png)|*.png";
-
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                pathFileImage = openFileDialog.FileName;
-                MessageBox.Show(pathFileImage);
-            }
-        }
-
-        private void createFolderImage()
-        {
-            string fileDestinyOriginal = Path.Combine(Directory.GetCurrentDirectory(), "img");
-
-            // Crear la carpeta "img" si no existe.
-            if (!Directory.Exists(fileDestinyOriginal))
-            {
-                Directory.CreateDirectory(fileDestinyOriginal);
-            }
-        }
+      InitializeComponent();
     }
+
+    public string GetUrlBifrostServer()
+    {
+      if (!string.IsNullOrEmpty(pathFileImage))
+      {
+        string fileDestiny = Path.Combine(Directory.GetCurrentDirectory(), "img");
+
+        createFolderImage();
+
+        string nameFileDestiny = Path.Combine(fileDestiny, "logo" + Path.GetExtension(pathFileImage));
+
+        try
+        {
+          if (File.Exists(nameFileDestiny))
+          {
+            File.Delete(nameFileDestiny);
+          }
+
+          File.Copy(pathFileImage, nameFileDestiny);
+          MessageBox.Show("Imagen guardada correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show($"Error al guardar la imagen: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      }
+
+      string ruc = textRUC.Text.Trim();
+      string suffixCompany = textSuffix.Text.Trim();
+      string domainBifrost = textDomain.Text.Trim();
+      string namespaceBifrost = textNamespace.Text.Trim();
+      return String.Format("{0}/{1}-{2}-{3}", domainBifrost, namespaceBifrost, ruc, suffixCompany);
+    }
+
+    private void OnCancel(object sender, EventArgs e)
+    {
+      Dispose();
+      Application.Exit();
+    }
+
+    private void btnPathImage_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog openFileDialog = new OpenFileDialog();
+      openFileDialog.Filter = "Archivo de imagen (*.png)|*.png";
+
+      if (openFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        pathFileImage = openFileDialog.FileName;
+        MessageBox.Show(pathFileImage);
+      }
+    }
+
+    private void createFolderImage()
+    {
+      string fileDestinyOriginal = Path.Combine(Directory.GetCurrentDirectory(), "img");
+
+      // Crear la carpeta "img" si no existe.
+      if (!Directory.Exists(fileDestinyOriginal))
+      {
+        Directory.CreateDirectory(fileDestinyOriginal);
+      }
+    }
+
+    private void OnClose(object sender, FormClosedEventArgs e)
+    {
+			if(e.CloseReason == CloseReason.UserClosing){
+				Application.Exit();
+			}
+    }
+  }
 }
